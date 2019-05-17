96a3e0d01839e746c8c32c284f7263bd2382de0d
zakram <zakram@IN5CG5130FK1.CSCMWS.CSCMWS.COM>
2019-05-17 11:03:41 +0530

    Updating file

---
+++ src/main/java/com/example/javamavenjunithelloworld/TestMain.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunithelloworld;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing ");
+
+	}
+
+}
